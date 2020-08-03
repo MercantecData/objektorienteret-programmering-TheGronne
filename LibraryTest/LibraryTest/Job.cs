d2b70cfb945f041e7bd7437a06cb4fc6054b67e8
Grønne <kegr.skp@edu.mercantec.dk>
2020-08-03 13:46:00 +0200

    Libraryy

---
+++ LibraryTest/LibraryTest/Job.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryTest
+{
+    public class Job
+    {
+        public string name;
+        public string title;
+        public string address;
+        public int monthlyPay;
+        public int weeklyWorkHours;
+
+        public Job(string name, string title, string address, int monthlyPay, int weeklyWorkHours)
+        {
+            this.name = name;
+            this.title = title;
+            this.address = address;
+            this.monthlyPay = monthlyPay;
+            this.weeklyWorkHours = weeklyWorkHours;
+        }
+    }
+}
---
+++ LibraryTest/LibraryTest/People.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryTest
+{
+    public class People
+    {
+        public List<Person> people = new List<Person>();
+        public void addToList(Person person)
+        {
+            this.people.Add(person);
+        }
+    }
+}
