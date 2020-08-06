d81db7afb2530893d8ebc708bf009c47fd8c5f20
Kasper Eliassen Grønne <kegr.skp@edu.mercantec.dk>
2020-08-06 12:53:37 +0200

    Er i gang med at se om der er noget jeg angler/skal have fikset

--- SchoolLibrary/SchoolLibrary/Collection.cs
+++ SchoolLibrary/SchoolLibrary/Collection.cs
@@ -1,28 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace SchoolLibrary
 {
     public class Collection
     {
-        private List<Student> students = new List<Student>();
-        List<Teacher> teachers = new List<Teacher>();
-        List<Team> teams = new List<Team>();
+        public List<Student> students = new List<Student>();
+        public List<Employee> teachers = new List<Employee>();
+        public List<Team> teams = new List<Team>();
+        int currentStudentID;
+        int currentTeacherID;
+        int currentTeamID;
 
-        public Collection(List<Student> students, List<Teacher> teachers, List<Team> teams)
+        public Collection(List<Student> students, List<Employee> teachers, List<Team> teams)
         {
             this.students = students;
             this.teachers = teachers;
             this.teams = teams;
         }
 
-        public void getAllStudents(List<Student> list)
+
+        //adds student
+        public void add(string name, int age)
         {
-            var students = new List<Student>();
-            for (int i = 0; i < list.Count; i++)
+            Student student = new Student(name, currentStudentID, age, new List<Team>());
+            students.Add(student);
+            currentStudentID++;
+        }
+        //removes student
+        public void remove(Student student)
+        {
+            foreach (var Student in teams.Where(x => x.students.Contains(student)))
             {
-                Console.WriteLine(students[i].nam)
+                remove(student);
             }
+            students.Remove(student);
+        }
+
+        //adds teacher
+        public void add(string name, decimal earnings, int paymentDay, bool payed, bool isTeacher)
+        {
+            Employee teacher = new Employee(name, currentTeacherID, new List<Team>(), earnings, paymentDay, payed, isTeacher);
+            teachers.Add(teacher);
+            currentTeacherID++;
+        }
+        //removes teacher
+        public void remove(Employee teacher)
+        {
+            foreach (var Teacher in teams.Where(x => x.teachers.Contains(teacher)))
+            {
+                remove(teacher);
+            }
+            teachers.Remove(teacher);
+        }
+
+        //adds team
+        public void add(string name)
+        {
+            Team team = new Team(name, currentTeamID, new List<Student>(), new List<Employee>());
+            teams.Add(team);
+            currentTeamID++;
+        }
+        //removes team
+        public void remove(Team team)
+        {
+            foreach (var Team in students.Where(x => x.teams.Contains(team)))
+            {
+                remove(team);
+            }
+            foreach (var Team in teachers.Where(x => x.teams.Contains(team)))
+            {
+                remove(team);
+            }
+            teams.Remove(team);
+        }
+
+        //returns a list of all students in the school
+        public List<Student> getStudents()
+        {
+            return students;
+        }
+        //returns a list of all teachers in the school
+        public List<Employee> getTeachers()
+        {
+            return teachers;
+        }
+        //returns a list of all teams in the school
+        public List<Team> getTeams()
+        {
+            
+            return teams;
         }
     }
 }
---
+++ SchoolLibrary/SchoolLibrary/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Employee
+    {
+        public string name;
+        public int id;
+        public List<Team> teams = new List<Team>();
+        public decimal earnings;
+        public int paymentDay;
+        public bool payed;
+        public bool isTeacher;
+
+        public Employee(string name, int id, List<Team> teams, decimal earnings, int paymentDay, bool payed, bool isTeacher)
+        {
+            this.name = name;
+            this.id = id;
+            this.teams = teams;
+            this.earnings = earnings;
+            this.paymentDay = paymentDay;
+            this.payed = payed;
+            this.isTeacher = isTeacher;
+        }
+
+
+        //Changes name of teacher
+        public void edit(string name)
+        {
+            this.name = name;
+        }
+        //changes earnings/month of teacher
+        public void edit(decimal earnings)
+        {
+            this.earnings = earnings;
+        }
+        //changes payment day of month of teacher
+        public void edit(int paymentDay)
+        {
+            this.paymentDay = paymentDay;
+        }
+        //changes if teacher has been payed this month
+        public void edit(bool payed)
+        {
+            this.payed = payed;
+        }
+
+        //returns a list of all teams the teacher is in
+        public List<Team> getTeams()
+        {
+            return teams;
+        }
+        //returns all current values of the teacher as a string
+        public string getProfile()
+        {
+            string list = "";
+            string list2 = "";
+            for (int i = 0; i < teams.Count; i++)
+            {
+                list = teams[i].name;
+                list2 = list2 + ", " + list;
+            }
+            return "Name: " + this.name + ", ID: " + this.id + ", Teams: " + list2 + ", Earnings: " + this.earnings + ", Payment Day: " + this.paymentDay + ", Payed: " + payed + ", " + isTeacher + ".";
+        }
+    }
+}
